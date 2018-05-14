57fc6064a769846dcb9b540e8de139487df1b644
efsa <mit@efsa.unsa.ba>
2018-05-14 14:37:45 +0200

    Finitoo
    Finitoo

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ root "pages#landing_page"
 
 get '/druga' => 'pages#druga'
 get '/clanstvo' => 'pages#clanstvo'
+get '/zanimljivosti' => 'pages#zanimljivosti'
+get '/forum' => 'pages#forum'
+get '/rezervacija' => 'pages#rezervacija'
 
 
 end
